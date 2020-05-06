01dbff64fdcae77c80eb5e30ccb28f97f419bcdd
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-06 20:38:32 +0530

    add the equality check expression evaluator

---
+++ src/expression-evaluators/boolean-expressions/double-equals.ts
@@ -0,0 +1,39 @@
+import { Scope } from "src/models/Scope";
+import { ExpressionEvaluator } from "src/expression-evaluators/expression-evaluator";
+import { bugReporter } from "src/language-bug-handling";
+import { NumberEvaluator } from "src/expression-evaluators/arithmetic-expressions/number-evaluator";
+
+export class DoubleEquals extends ExpressionEvaluator {
+    private identifier: string = '==';
+
+    constructor(public scope: Scope) {
+        super();
+    }
+
+    tryEvaluate(text: string): boolean {
+        return text.includes(this.identifier);
+    }
+
+    evaluate(text: string): any {
+        if (this.tryEvaluate(text)) {
+            const individualComponents = text.split(this.identifier);
+            const lhs = individualComponents[0];
+            const rhs = individualComponents[1];
+
+            const numberEvaluator = new NumberEvaluator(this.scope);
+
+            if (numberEvaluator.tryEvaluate(lhs) === false ||
+                numberEvaluator.tryEvaluate(rhs) === false) {
+                throw new Error("Can't compare non-number symbols.");
+            }
+
+            const lhsNumber = numberEvaluator.evaluate(lhs);
+            const rhsNumber = numberEvaluator.evaluate(rhs);
+
+            return lhsNumber === rhsNumber;
+        } else {
+            bugReporter.report('INVALID_DOUBLE_EQUALITY_COMPARISION');
+        }
+    }
+
+}
---
+++ tests/expressions-evaluator/boolean-expressions/double-equals.spec.ts
@@ -0,0 +1,75 @@
+import { DoubleEquals } from "src/expression-evaluators/boolean-expressions/double-equals";
+import { Scope } from "src/models/Scope";
+import { VariableBlock, VariableBlockType } from "src/blocks/variable-block";
+
+describe('check the tryEvaluate functionality of double equals.', () => {
+
+    let scope: Scope;
+    let doubleEquals: DoubleEquals;
+    beforeEach(() => {
+        scope = new Scope();
+        doubleEquals = new DoubleEquals(scope)
+    });
+
+    test('should return false if input is empty.', () => {
+        const input = "";
+        const result = doubleEquals.tryEvaluate(input);
+
+        expect(result).toStrictEqual(false);
+    });
+
+    test('should return false if input is not a valid double-equals expression.', () => {
+        const input = "1 != 2";
+        const result = doubleEquals.tryEvaluate(input);
+
+        expect(result).toStrictEqual(false);
+    });
+
+    test('should return true if input is a valid double-equals expression.', () => {
+        const input = "1 == 2";
+        const result = doubleEquals.tryEvaluate(input);
+
+        expect(result).toStrictEqual(true);
+    });
+});
+
+describe('check the tryEvaluate functionality of double equals.', () => {
+
+    let scope: Scope;
+    let doubleEquals: DoubleEquals;
+    beforeEach(() => {
+        scope = new Scope();
+        doubleEquals = new DoubleEquals(scope)
+    });
+
+    test('should return false for non-equal numbers equality comparision.', () => {
+        const input = " 2 == 56";
+        const result = doubleEquals.evaluate(input);
+
+        expect(result).toStrictEqual(false);
+    });
+
+    test('should return true for equal numbers equality comparision.', () => {
+        const input = " 42 == 42";
+        const result = doubleEquals.evaluate(input);
+
+        expect(result).toStrictEqual(true);
+    });
+
+    test('should compare variable numbers.', () => {
+        const input = " someVariable1 == 100";
+
+        const variableBlock = new VariableBlock(
+            VariableBlockType.declare,
+            'someVariable1',
+            'number',
+            100,
+            true,
+            scope
+        );
+        variableBlock.execute();
+
+        const result = doubleEquals.evaluate(input);
+        expect(result).toStrictEqual(true);
+    });
+});
