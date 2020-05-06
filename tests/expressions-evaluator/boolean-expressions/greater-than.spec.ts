4c248ebe0a202e7518767a0756efd196776d4904
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-06 20:56:41 +0530

    add the greater than check, expression evaluator

---
+++ tests/expressions-evaluator/boolean-expressions/greater-than.spec.ts
@@ -0,0 +1,75 @@
+import { Scope } from "src/models/Scope";
+import { VariableBlock, VariableBlockType } from "src/blocks/variable-block";
+import { GreaterThan } from "src/expression-evaluators/boolean-expressions/greater-than";
+
+describe('check the tryEvaluate functionality of double equals.', () => {
+
+    let scope: Scope;
+    let greaterThan: GreaterThan;
+    beforeEach(() => {
+        scope = new Scope();
+        greaterThan = new GreaterThan(scope)
+    });
+
+    test('should return false if input is empty.', () => {
+        const input = "";
+        const result = greaterThan.tryEvaluate(input);
+
+        expect(result).toStrictEqual(false);
+    });
+
+    test('should return false if input is not a valid greater-than expression.', () => {
+        const input = "1 != 2";
+        const result = greaterThan.tryEvaluate(input);
+
+        expect(result).toStrictEqual(false);
+    });
+
+    test('should return true if input is a valid greater-than expression.', () => {
+        const input = "1 > 2";
+        const result = greaterThan.tryEvaluate(input);
+
+        expect(result).toStrictEqual(true);
+    });
+});
+
+describe('check the evaluate functionality of greater-than.', () => {
+
+    let scope: Scope;
+    let greaterThan: GreaterThan;
+    beforeEach(() => {
+        scope = new Scope();
+        greaterThan = new GreaterThan(scope)
+    });
+
+    test('should return false for incorrect greater than comparision.', () => {
+        const input = " 2 > 56";
+        const result = greaterThan.evaluate(input);
+
+        expect(result).toStrictEqual(false);
+    });
+
+    test('should return true for correct greater than comparision.', () => {
+        const input = " 42 > -41";
+        const result = greaterThan.evaluate(input);
+
+        expect(result).toStrictEqual(true);
+    });
+
+    test('should compare variable numbers.', () => {
+        const input = " someVariable1 > 100";
+
+        const variableBlock = new VariableBlock(
+            VariableBlockType.declare,
+            'someVariable1',
+            'number',
+            101,
+            true,
+            scope
+        );
+        variableBlock.execute();
+
+        const result = greaterThan.evaluate(input);
+        expect(result).toStrictEqual(true);
+    });
+});
