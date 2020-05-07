553664b87f0979e6e1944815ea9202e8eb6a588d
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-07 22:15:49 +0530

    add the multiplication expression evaluation functionality

---
+++ src/expression-evaluators/arithmetic-expressions/multiplication-evaluator.ts
@@ -0,0 +1,30 @@
+import { ExpressionEvaluator } from "src/expression-evaluators/expression-evaluator";
+import { Scope } from "src/models/Scope";
+import { bugReporter } from "src/language-bug-handling";
+import { arithmeticOperationEvaluator } from "src/expression-evaluators/arithmetic-expressions/arithmetic-operation-evaluator";
+
+export class MultiplicationEvaluator extends ExpressionEvaluator {
+    private identifier: string = '*';
+
+    constructor(public scope: Scope) {
+        super();
+    }
+
+    tryEvaluate(text: string): boolean {
+        return text.includes(this.identifier);
+    }
+
+    evaluate(text: string): any {
+        if (this.tryEvaluate(text)) {
+            return arithmeticOperationEvaluator(
+                text,
+                this.identifier,
+                this.scope,
+                (lhs, rhs) => lhs * rhs
+            );
+        } else {
+            bugReporter.report('INVALID_MULTIPLICATION_OPERATION');
+        }
+    }
+
+}
