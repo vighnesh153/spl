22c792625321951a58392753af0d19ab0b1814ad
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-09 08:21:48 +0530

    add the array variable block

---
+++ src/blocks/variable-blocks/array-variable-block.ts
@@ -0,0 +1,26 @@
+import { VariableBlock, VariableBlockType } from "src/blocks/variable-blocks/variable-block";
+import { Scope } from "src/models/Scope";
+
+export class ArrayVariableBlock extends VariableBlock {
+    constructor(public typeOfBlock: VariableBlockType,
+                public variableName: string,
+                public typeOfVariable: string,
+                public value: any,
+                public isPermanent: boolean,
+                parentScope: Scope,
+                public typeOfArray: 'string' | 'number' | 'boolean') {
+        super(typeOfBlock, variableName, typeOfVariable, value, isPermanent, parentScope);
+    }
+
+    protected set(): void {
+        if (this.scope.hasVariable(this.variableName) === false) {
+            throw new Error('Trying to set value of not-declared variable.')
+        } else {
+            const variable = this.scope.getVariable(this.variableName);
+            if (variable.isPermanent) {
+                throw new Error('Tried to modify a permanent variable.')
+            }
+            this.scope.getVariable(this.variableName).value = this.value;
+        }
+    }
+}
