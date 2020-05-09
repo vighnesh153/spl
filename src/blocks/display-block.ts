a90ad98f4f03bb2b2acd74034f09f19b4f8507c5
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-09 19:49:34 +0530

    modify the display block to use expression evaluators instead of parsers

--- src/blocks/display-block.ts
+++ src/blocks/display-block.ts
@@ -8,20 +8,25 @@ import { BooleanParser } from "src/parsers/data-type-parsers/primitive-parsers/b
 import { NumberParser } from "src/parsers/data-type-parsers/primitive-parsers/number-parser";
 import { StringParser } from "src/parsers/data-type-parsers/primitive-parsers/string-parser";
 import { ArrayParser } from "src/parsers/data-type-parsers/non-primitive-parsers/array-parser";
+import { ExpressionEvaluator } from "src/expression-evaluators/expression-evaluator";
+import { ArrayExpressionEvaluator } from "src/expression-evaluators/array-expression-evaluator";
+import { ArithmeticExpressionEvaluator } from "src/expression-evaluators/arithmetic-expressions/arithmetic-expression-evaluator";
+import { StringExpressionEvaluator } from "src/expression-evaluators/string-expression-evaluator";
+import { BooleanExpressionEvaluator } from "src/expression-evaluators/boolean-expressions/boolean-expression-evaluator";
 
 export class DisplayBlock extends Block {
     scope: Scope;
 
-    private static parsers: DatatypeParser[] = [
-        BooleanParser.instance,
-        NumberParser.instance,
-        StringParser.instance,
-        ArrayParser.instance
-    ];
+    private readonly expressionEvaluators: ExpressionEvaluator[] = [];
 
     constructor(private values: string[], parentScope: Scope) {
         super();
         this.scope = parentScope;
+
+        this.expressionEvaluators.push(new ArrayExpressionEvaluator(this.scope));
+        this.expressionEvaluators.push(new BooleanExpressionEvaluator(this.scope));
+        this.expressionEvaluators.push(new ArithmeticExpressionEvaluator(this.scope));
+        this.expressionEvaluators.push(new StringExpressionEvaluator(this.scope));
     }
 
     execute(): void {
@@ -29,10 +34,10 @@ export class DisplayBlock extends Block {
 
         for (const value of this.values) {
             let isLiteral = false;
-            for (const parser of DisplayBlock.parsers) {
-                if (parser.tryParse(value)) {
+            for (const evaluator of this.expressionEvaluators) {
+                if (evaluator.tryEvaluate(value)) {
                     isLiteral = true;
-                    const stringForm = parser.parse(value).toString();
+                    const stringForm = evaluator.evaluate(value).toString();
                     outputBuffer.push(stringForm);
                     break;
                 }
