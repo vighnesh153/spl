251403e2f0e13de87a2198591c1dbbb49e9e10d7
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-10 17:02:23 +0530

    add the lopp while expression is true block

---
+++ src/blocks/loop-blocks/loop-while-expression-is-true.ts
@@ -0,0 +1,33 @@
+import { Block } from "src/blocks/Block";
+import { Scope } from "src/models/Scope";
+import { LineOfCode } from "src/models/LineOfCode";
+import { Interpreter } from "src/interpreter";
+import { BooleanExpressionEvaluator } from "src/expression-evaluators/boolean-expressions/boolean-expression-evaluator";
+
+export class LoopWhileExpressionIsTrue extends Block {
+    scope: Scope;
+
+    constructor(private expression: string,
+                parentScope: Scope,
+                private childLinesOfCode: LineOfCode[]) {
+        super();
+        this.scope = new Scope(parentScope);
+    }
+
+    execute(): void {
+        const evaluator = new BooleanExpressionEvaluator(this.scope);
+
+        const throwIfExpressionIsInvalid = () => {
+            if (evaluator.tryEvaluate(this.expression) === false) {
+                throw new Error('Invalid boolean expression.');
+            }
+        }
+
+        throwIfExpressionIsInvalid();
+        while (evaluator.evaluate(this.expression)) {
+            new Interpreter(this.childLinesOfCode.slice(), this.scope).interpret();
+            throwIfExpressionIsInvalid();
+        }
+    }
+
+}
---
+++ tests/blocks/loop-blocks/loop-while-expression-is-true.spec.ts
@@ -0,0 +1,42 @@
+import { Scope } from "src/models/Scope";
+import { LineOfCode } from "src/models/LineOfCode";
+import { OutputBuffer } from "src/models/OutputBuffer";
+import { LoopWhileExpressionIsTrue } from "src/blocks/loop-blocks/loop-while-expression-is-true";
+import { VariableBlock, VariableBlockType } from "src/blocks/variable-blocks/variable-block";
+
+describe('check the functionality of loop while expression is true', () => {
+
+    let scope: Scope;
+    let linesOfCode: LineOfCode[];
+    let block: LoopWhileExpressionIsTrue;
+    beforeEach(() => {
+        scope = new Scope();
+        linesOfCode = [];
+
+        const variableBlock = new VariableBlock(
+            VariableBlockType.declare,
+            'x',
+            'number',
+            0,
+            false,
+            scope
+        );
+        variableBlock.execute();
+    });
+
+    const addLineOfCode = (line: string) => {
+        linesOfCode.push(new LineOfCode(line, Math.random()));
+    }
+
+    test('should print something while expression is true', () => {
+        addLineOfCode('    display \'Hello\'');
+        addLineOfCode('    set x to x + 1');
+
+        block = new LoopWhileExpressionIsTrue('x < 5', scope, linesOfCode);
+        block.execute();
+
+        const result = OutputBuffer.instance.getAndFlush();
+        expect(result).toStrictEqual('Hello\nHello\nHello\nHello\nHello\n');
+    });
+
+});
