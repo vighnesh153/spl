0835dddfb966bf2d1fbaf5cfed425e56783fa312
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-12 07:34:17 +0530

    add the function definition block parser

---
+++ tests/parsers/block-parsers/functions/function-definition-block-parser.spec.ts
@@ -0,0 +1,30 @@
+import { FunctionDefinitionBlockParser } from "src/parsers/block-parsers/functions/function-definition-block-parser";
+import { Scope } from "src/models/Scope";
+import { LineOfCode } from "src/models/LineOfCode";
+
+describe('check the functionality of function definition block parser.', () => {
+
+    let scope: Scope;
+    let linesOfCode: LineOfCode[];
+    beforeEach(() => {
+        scope = new Scope();
+        linesOfCode = [];
+    });
+
+    const addLineOfCode = (line: string) => {
+        linesOfCode.push(new LineOfCode(line, Math.random()));
+    }
+
+    it('should define a function.', () => {
+        addLineOfCode('define function func with arguments [] which returns nothing:');
+        addLineOfCode('    display \'Hello\'');
+        linesOfCode.reverse();
+
+        const parser = new FunctionDefinitionBlockParser(scope, linesOfCode);
+        const block = parser.parse();
+        block.execute();
+
+        expect(scope.hasFunction('func')).toStrictEqual(true);
+    });
+
+});
