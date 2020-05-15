7297ff199915aa0f0ac5e09d9e9e9e2a66593fa3
vighnesh153 <vighnesh.raut13@gmail.com>
2020-05-15 13:38:30 +0530

    add the examples, added in the UI, to the main.spec

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -61,7 +61,7 @@ display fibNums
         expect(main.getOutput()).toStrictEqual('0,1,1,2,3,5,8,13,21,34,55,89\n');
     });
 
-    test('should check for primality.', () => {
+    test('should print prime numbers.', () => {
         const code = `
 
 
@@ -146,4 +146,329 @@ loop while num <= 50:
             '50: false\n');
     });
 
+    test('should find the factorial of numbers.', () => {
+        const code = `
+define function factorial with arguments [ number n ] which returns number:
+\tif n == 0 or n == 1, then do:
+\t\treturn n
+\t
+\tlet number factNMinus1 be result of factorial(n - 1)
+\treturn n * factNMinus1
+
+\t\t
+let number num be 1
+loop while num <=10:
+\tlet number res be result of factorial(num)
+\tdisplay num, ' factorial is ', res
+\tset num to num + 1
+
+`;
+        const main = new Main(code);
+        main.compile(new Scope());
+
+        expect(main.getOutput()).toStrictEqual('1 factorial is 1\n' +
+            '2 factorial is 2\n' +
+            '3 factorial is 6\n' +
+            '4 factorial is 24\n' +
+            '5 factorial is 120\n' +
+            '6 factorial is 720\n' +
+            '7 factorial is 5040\n' +
+            '8 factorial is 40320\n' +
+            '9 factorial is 362880\n' +
+            '10 factorial is 3628800\n');
+    });
+
+    test('should sort the array of numbers.', () => {
+        const code = `
+define function bubbleSort with arguments [ array of number arr ] which returns array of number:
+\tlet number i be 0
+\tlet number arrLen be length of arr
+\tloop while i < arrLen:
+\t\tlet number j be 0
+\t\tloop while j+1 < arrLen:
+\t\t\tif arr[j+1] < arr[j], then do:
+\t\t\t\tlet number temp be arr[j]
+\t\t\t\tset arr[j] to arr[j+1]
+\t\t\t\tset arr[j+1] to temp
+\t\t\tset j to j + 1
+\t\tset i to i + 1
+\treturn arr
+
+let array of number, myNumberArray, be [ 34, 42, 12, 45, 64, 22, 21, 43, 2, 23, 1, 0]
+let array of number, sortedArray, be result of bubbleSort(myNumberArray)
+display sortedArray
+
+        `;
+// [ 34, 42, 12, 45, 64, 22, 21, 43, 2, 23, 1, 0]
+        const main = new Main(code);
+        main.compile(new Scope());
+
+        expect(main.getOutput()).toStrictEqual('0,1,2,12,21,22,23,34,42,43,45,64\n');
+    });
+
+    test('should evaluate nested loop.', () => {
+        const code = `
+loop for 5 times with i as counter:
+\tloop for 6 times with j as counter:
+\t\tloop for 7 times with k as counter:
+\t\t\tdisplay i, ', ', j, ', ', k
+        `;
+
+        const main = new Main(code);
+        main.compile(new Scope());
+
+        expect(main.getOutput()).toStrictEqual('1, 1, 1\n' +
+            '1, 1, 2\n' +
+            '1, 1, 3\n' +
+            '1, 1, 4\n' +
+            '1, 1, 5\n' +
+            '1, 1, 6\n' +
+            '1, 1, 7\n' +
+            '1, 2, 1\n' +
+            '1, 2, 2\n' +
+            '1, 2, 3\n' +
+            '1, 2, 4\n' +
+            '1, 2, 5\n' +
+            '1, 2, 6\n' +
+            '1, 2, 7\n' +
+            '1, 3, 1\n' +
+            '1, 3, 2\n' +
+            '1, 3, 3\n' +
+            '1, 3, 4\n' +
+            '1, 3, 5\n' +
+            '1, 3, 6\n' +
+            '1, 3, 7\n' +
+            '1, 4, 1\n' +
+            '1, 4, 2\n' +
+            '1, 4, 3\n' +
+            '1, 4, 4\n' +
+            '1, 4, 5\n' +
+            '1, 4, 6\n' +
+            '1, 4, 7\n' +
+            '1, 5, 1\n' +
+            '1, 5, 2\n' +
+            '1, 5, 3\n' +
+            '1, 5, 4\n' +
+            '1, 5, 5\n' +
+            '1, 5, 6\n' +
+            '1, 5, 7\n' +
+            '1, 6, 1\n' +
+            '1, 6, 2\n' +
+            '1, 6, 3\n' +
+            '1, 6, 4\n' +
+            '1, 6, 5\n' +
+            '1, 6, 6\n' +
+            '1, 6, 7\n' +
+            '2, 1, 1\n' +
+            '2, 1, 2\n' +
+            '2, 1, 3\n' +
+            '2, 1, 4\n' +
+            '2, 1, 5\n' +
+            '2, 1, 6\n' +
+            '2, 1, 7\n' +
+            '2, 2, 1\n' +
+            '2, 2, 2\n' +
+            '2, 2, 3\n' +
+            '2, 2, 4\n' +
+            '2, 2, 5\n' +
+            '2, 2, 6\n' +
+            '2, 2, 7\n' +
+            '2, 3, 1\n' +
+            '2, 3, 2\n' +
+            '2, 3, 3\n' +
+            '2, 3, 4\n' +
+            '2, 3, 5\n' +
+            '2, 3, 6\n' +
+            '2, 3, 7\n' +
+            '2, 4, 1\n' +
+            '2, 4, 2\n' +
+            '2, 4, 3\n' +
+            '2, 4, 4\n' +
+            '2, 4, 5\n' +
+            '2, 4, 6\n' +
+            '2, 4, 7\n' +
+            '2, 5, 1\n' +
+            '2, 5, 2\n' +
+            '2, 5, 3\n' +
+            '2, 5, 4\n' +
+            '2, 5, 5\n' +
+            '2, 5, 6\n' +
+            '2, 5, 7\n' +
+            '2, 6, 1\n' +
+            '2, 6, 2\n' +
+            '2, 6, 3\n' +
+            '2, 6, 4\n' +
+            '2, 6, 5\n' +
+            '2, 6, 6\n' +
+            '2, 6, 7\n' +
+            '3, 1, 1\n' +
+            '3, 1, 2\n' +
+            '3, 1, 3\n' +
+            '3, 1, 4\n' +
+            '3, 1, 5\n' +
+            '3, 1, 6\n' +
+            '3, 1, 7\n' +
+            '3, 2, 1\n' +
+            '3, 2, 2\n' +
+            '3, 2, 3\n' +
+            '3, 2, 4\n' +
+            '3, 2, 5\n' +
+            '3, 2, 6\n' +
+            '3, 2, 7\n' +
+            '3, 3, 1\n' +
+            '3, 3, 2\n' +
+            '3, 3, 3\n' +
+            '3, 3, 4\n' +
+            '3, 3, 5\n' +
+            '3, 3, 6\n' +
+            '3, 3, 7\n' +
+            '3, 4, 1\n' +
+            '3, 4, 2\n' +
+            '3, 4, 3\n' +
+            '3, 4, 4\n' +
+            '3, 4, 5\n' +
+            '3, 4, 6\n' +
+            '3, 4, 7\n' +
+            '3, 5, 1\n' +
+            '3, 5, 2\n' +
+            '3, 5, 3\n' +
+            '3, 5, 4\n' +
+            '3, 5, 5\n' +
+            '3, 5, 6\n' +
+            '3, 5, 7\n' +
+            '3, 6, 1\n' +
+            '3, 6, 2\n' +
+            '3, 6, 3\n' +
+            '3, 6, 4\n' +
+            '3, 6, 5\n' +
+            '3, 6, 6\n' +
+            '3, 6, 7\n' +
+            '4, 1, 1\n' +
+            '4, 1, 2\n' +
+            '4, 1, 3\n' +
+            '4, 1, 4\n' +
+            '4, 1, 5\n' +
+            '4, 1, 6\n' +
+            '4, 1, 7\n' +
+            '4, 2, 1\n' +
+            '4, 2, 2\n' +
+            '4, 2, 3\n' +
+            '4, 2, 4\n' +
+            '4, 2, 5\n' +
+            '4, 2, 6\n' +
+            '4, 2, 7\n' +
+            '4, 3, 1\n' +
+            '4, 3, 2\n' +
+            '4, 3, 3\n' +
+            '4, 3, 4\n' +
+            '4, 3, 5\n' +
+            '4, 3, 6\n' +
+            '4, 3, 7\n' +
+            '4, 4, 1\n' +
+            '4, 4, 2\n' +
+            '4, 4, 3\n' +
+            '4, 4, 4\n' +
+            '4, 4, 5\n' +
+            '4, 4, 6\n' +
+            '4, 4, 7\n' +
+            '4, 5, 1\n' +
+            '4, 5, 2\n' +
+            '4, 5, 3\n' +
+            '4, 5, 4\n' +
+            '4, 5, 5\n' +
+            '4, 5, 6\n' +
+            '4, 5, 7\n' +
+            '4, 6, 1\n' +
+            '4, 6, 2\n' +
+            '4, 6, 3\n' +
+            '4, 6, 4\n' +
+            '4, 6, 5\n' +
+            '4, 6, 6\n' +
+            '4, 6, 7\n' +
+            '5, 1, 1\n' +
+            '5, 1, 2\n' +
+            '5, 1, 3\n' +
+            '5, 1, 4\n' +
+            '5, 1, 5\n' +
+            '5, 1, 6\n' +
+            '5, 1, 7\n' +
+            '5, 2, 1\n' +
+            '5, 2, 2\n' +
+            '5, 2, 3\n' +
+            '5, 2, 4\n' +
+            '5, 2, 5\n' +
+            '5, 2, 6\n' +
+            '5, 2, 7\n' +
+            '5, 3, 1\n' +
+            '5, 3, 2\n' +
+            '5, 3, 3\n' +
+            '5, 3, 4\n' +
+            '5, 3, 5\n' +
+            '5, 3, 6\n' +
+            '5, 3, 7\n' +
+            '5, 4, 1\n' +
+            '5, 4, 2\n' +
+            '5, 4, 3\n' +
+            '5, 4, 4\n' +
+            '5, 4, 5\n' +
+            '5, 4, 6\n' +
+            '5, 4, 7\n' +
+            '5, 5, 1\n' +
+            '5, 5, 2\n' +
+            '5, 5, 3\n' +
+            '5, 5, 4\n' +
+            '5, 5, 5\n' +
+            '5, 5, 6\n' +
+            '5, 5, 7\n' +
+            '5, 6, 1\n' +
+            '5, 6, 2\n' +
+            '5, 6, 3\n' +
+            '5, 6, 4\n' +
+            '5, 6, 5\n' +
+            '5, 6, 6\n' +
+            '5, 6, 7\n');
+    });
+
+    test('should evaluate the fizz-buzz code.', () => {
+        const code = `
+define function fizzBuzz with arguments [ number n ] which returns nothing:
+\tloop for n times with i as counter:
+\t\tif i%15 == 0, then do:
+\t\t\tdisplay 'FizzBuzz'
+\t\telse if i%3 == 0, then do:
+\t\t\tdisplay 'Fizz'
+\t\telse if i%5 == 0, then do:
+\t\t\tdisplay 'Buzz'
+\t\telse, do:
+\t\t\tdisplay i
+
+\t\t\t
+execute fizzBuzz(20)
+ 
+        `;
+
+        const main = new Main(code);
+        main.compile(new Scope());
+
+        expect(main.getOutput()).toStrictEqual('1\n' +
+            '2\n' +
+            'Fizz\n' +
+            '4\n' +
+            'Buzz\n' +
+            'Fizz\n' +
+            '7\n' +
+            '8\n' +
+            'Fizz\n' +
+            'Buzz\n' +
+            '11\n' +
+            'Fizz\n' +
+            '13\n' +
+            '14\n' +
+            'FizzBuzz\n' +
+            '16\n' +
+            '17\n' +
+            'Fizz\n' +
+            '19\n' +
+            'Buzz\n');
+    });
 });
